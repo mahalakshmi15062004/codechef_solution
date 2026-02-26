02b6834d5597736b5d3faf78f638cc341bce8e04
Mahalakshmi S <mahaappu125@gmail.com>
2026-02-26 16:00:47 +0530

    SquareNumbers.java

--- java_programming/easy/SquareNumbers.java
+++ java_programming/easy/SquareNumbers.java
@@ -1,11 +1,11 @@
 public class SquareNumbers {
     public static void main(String[] args) {
-        // Loop from 1 to 5
+       
         for (int i = 1; i <= 5; i++) {
-            // Calculate the square using multiplication
+           
             int square = i * i;
             
-            // Print the result in the requested format
+           
             System.out.println(i + " - " + square);
         }
     }
